021b3950ce7073e3f733d144c4b028df9f17589e
phucthang1101 <phucthangvt1101@gmail.com>
2022-02-01 16:03:12 -0500

    clean up to deploy

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button, Col, Container, Row } from 'react-bootstrap';
-import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import Header from '../../components/Header';
 import Layout from '../../components/Layout';
